d52e225626ee9cb0f18c8ebc59427b071142604d
xuxiancan <76151216+xuxiancan@users.noreply.github.com>
2025-07-04 15:39:15 +0800

    :art:  #3633【微信支付】创建支付分订单接口请求参数里增加设备信息字段

--- weixin-java-pay/src/test/java/com/github/binarywang/wxpay/bean/payscore/WxPartnerPayScoreRequestTest.java
+++ weixin-java-pay/src/test/java/com/github/binarywang/wxpay/bean/payscore/WxPartnerPayScoreRequestTest.java
@@ -8,10 +8,11 @@ import static org.assertj.core.api.Assertions.assertThat;
  * @author <a href="https://github.com/binarywang">Binary Wang</a>
  * created on  2020-07-11
  */
-public class WxPayScoreRequestTest {
+public class WxPartnerPayScoreRequestTest {
+
   @Test
   public void testToJson() {
-    WxPayScoreRequest request = WxPayScoreRequest.builder()
+    WxPartnerPayScoreRequest request = WxPartnerPayScoreRequest.builder()
       .outOrderNo("QLS202005201058000201")
       .appid("123")
       .serviceId("345")
@@ -19,10 +20,9 @@ public class WxPayScoreRequestTest {
       .timeRange(new TimeRange("20230901011023", "20230930235959","开始时间","结束时间"))
       .device(new Device("deviceId","deviceId","212323232"))
       .build();
-    String json = request.toJson();
-    System.out.println(json);
-
-    String expectedJson = "{\"out_order_no\":\"QLS202005201058000201\",\"appid\":\"123\",\"service_id\":\"345\",\"service_introduction\":\"租借服务\",\"time_range\":{\"start_time\":\"20230901011023\",\"end_time\":\"20230930235959\",\"start_time_remark\":\"开始时间\",\"end_time_remark\":\"结束时间\"},\"device\":{\"start_device_id\":\"deviceId\",\"end_device_id\":\"deviceId\",\"materiel_no\":\"212323232\"}}";
+    System.out.println(request.toJson());
+    String expectedJson =
+      "{\"out_order_no\":\"QLS202005201058000201\",\"appid\":\"123\",\"service_id\":\"345\",\"service_introduction\":\"租借服务\",\"time_range\":{\"start_time\":\"20230901011023\",\"end_time\":\"20230930235959\",\"start_time_remark\":\"开始时间\",\"end_time_remark\":\"结束时间\"},\"device\":{\"start_device_id\":\"deviceId\",\"end_device_id\":\"deviceId\",\"materiel_no\":\"212323232\"}}";
     assertThat(request.toJson()).isEqualTo(expectedJson);
 //    {
 //      "out_order_no": "QLS202005201058000201",
